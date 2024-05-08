636d364f2eaba794193974757af371605508509c
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-08 23:41:35 +0100

    chat update

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -2,7 +2,14 @@ import React, { useEffect, useRef, useState } from "react";
 import EmojiPicker from "emoji-picker-react";
 
 import "./chat.css";
+import { doc, onSnapshot } from "firebase/firestore";
+import { db } from "../../lib/firebase";
 const Chat = () => {
+
+  const { chatId } = useChatStore();
+
+
+  const [chat, setChat] = useState();
   const [open, setOpen] = useState(false);
   const [text, setText] = useState("");
 
@@ -11,6 +18,19 @@ const Chat = () => {
   useEffect(()=>{
     endRef.current?.scrollIntoView({behavior:'smooth'});
   })
+
+  useEffect(()=>{
+    const unSub = onSnapshot(doc(db, 'chats', chatId), (res)=>{
+      setChat(res.data());
+    })
+
+    return ()=>{
+      unSub()
+    }
+
+  },[chatId])
+  console.log(chat)
+
 
   const handleEmoji = (e) => {
     setText((prev) => prev + e.emoji);
