28261575573ad1f23baf22675a2bcf8b5457d6d9
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-08 23:41:25 +0100

    appuser update

--- src/components/AppUser/AppUser.jsx
+++ src/components/AppUser/AppUser.jsx
@@ -1,24 +1,91 @@
-import React from 'react'
-import './appuser.css'
-
-
+import React, { useState } from "react";
+import "./appuser.css";
+import "./appuser.css";
+import { arrayUnion, collection, doc, getDoc, getDocs, query, serverTimestamp, setDoc, updateDoc, where } from "firebase/firestore";
+import { db } from "../../lib/firebase";
+import { useUserStore } from "../../lib/userStore";
 
 const AppUser = () => {
+  const [user, setUser] = useState(null);
+
+  const {currentUser} = useUserStore();
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const username = formData.get("username");
+
+    try {
+      const userRef = collection(db, "users");
+
+      const q = query(userRef, where("username", "==", username));
+
+      const querySnapShot = await getDocs(q);
+
+      if (!querySnapShot.empty) {
+        setUser(querySnapShot.docs[0].data());
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleAdd = async () =>{
+const chatRef = collection(db, 'chats')
+const userChatRef = collection(db, 'userchats')
+
+
+    try{
+
+      const newChatRef = doc(chatRef)
+
+      await setDoc(newChatRef, {
+        createdAt: serverTimestamp(),
+        messages:[],
+      })
+
+      await updateDoc(doc(userChatRef, user.id),{
+        chats:arrayUnion({
+          chatId: newChatRef.id,  
+          lastMessage: '',
+          recieverId: currentUser.id,
+          updatedAt: Date.now()
+        })
+      })
+      
+
+      await updateDoc(doc(userChatRef, currentUser.id),{
+        chats:arrayUnion({
+          chatId: newChatRef.id,  
+          lastMessage: '',
+          recieverId: user.id,
+          updatedAt: Date.now()
+        })
+      })
+
+      console.log(newChatRef.id)
+    }catch(err){
+      console.log(err)
+    }
+  }
+
   return (
-    <div className='addUser'>
-      <form action="">
-        <input type="text" placeholder='Username' name='username'/>
+    <div className="addUser">
+      <form onSubmit={handleSearch}>
+        <input type="text" placeholder="Username" name="username" />
         <button>search</button>
-    </form>
-    <div className='user'>
-        <div className="detail">
-            <img src='./avatar.png' alt="" />
-            <span>Jane Joe</span>
+      </form>
+      {user && (
+        <div className="user">
+          <div className="detail">
+            <img src={user.avatar || "./avatar.png"} alt="" />
+            <span>{user.username}</span>
+          </div>
+          <button onClick={handleAdd}>Add User</button>
         </div>
-        <button>Add User</button>
+      )}
     </div>
-    </div>
-  )
-}
-import './appuser.css'
-export default AppUser
+  );
+};
+
+export default AppUser;
