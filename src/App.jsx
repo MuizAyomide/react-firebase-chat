30d69eca56043e906d7381d47d208dd8d4e7b69c
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-05-08 23:40:26 +0100

    app update

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,45 @@
-import Login from "./components/Login/Login"
-import Notification from "./components/Notification/Notification"
-import Chat from "./components/chat/Chat"
-import Details from "./components/details/Details"
-import List from "./components/list/List"
+import { useEffect } from "react";
+import Login from "./components/Login/Login";
+import Notification from "./components/Notification/Notification";
+import Chat from "./components/chat/Chat";
+import Details from "./components/details/Details";
+import List from "./components/list/List";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./lib/firebase";
+import { useUserStore } from "./lib/userStore";
+import { useChatStore } from "./lib/chatStore copy";
 
 const App = () => {
+  const { chatId } = useChatStore();
 
-  const user = false;
+  const { currentUser, isLoading, fetchUserInfo } = useUserStore();
 
+  useEffect(() => {
+    const unSub = onAuthStateChanged(auth, (user) => {
+      fetchUserInfo(user?.uid);
+    });
+
+    return () => {
+      unSub();
+    };
+  }, [fetchUserInfo]);
+
+  if (isLoading) return <div className="loading"> Loading...</div>;
 
   return (
-    <div className='container'>
-      {
-        user ? (
-          <>
-          <List/>
-          <Chat/>
-          <Details/>        
-          </>
-        ):(<Login/>)
-      }
-      <Notification/>
+    <div className="container">
+      {currentUser ? (
+        <>
+          <List />
+          {chatId && <Chat />}
+          {chatId && <Details />}
+        </>
+      ) : (
+        <Login /> 
+      )}
+      <Notification />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
